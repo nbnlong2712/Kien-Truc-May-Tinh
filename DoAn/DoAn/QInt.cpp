e86d069d711aef333cd27e38d1d99e4e0358082b
BaoSon <lqBaoSon@outlook.com>
2019-11-01 19:33:25 +0700

    + va n =

--- DoAn/DoAn/QInt.cpp
+++ DoAn/DoAn/QInt.cpp
@@ -9,15 +9,82 @@ QInt::QInt() {
 };
 QInt QInt::operator=(const QInt &a) {
 	QInt c;
-	for (int i = 0; i < 4; i++) {
-		data[i] = c.data[i] | a.data[i]; // or voi QInt da nhap
-	}
+	c.binary = a.binary;
+	//ScanQInt(c);
 	return *this;
 };
-QInt QInt::operator+(const QInt &a) {
-	QInt b;
-	return b;
+const QInt QInt::operator+(QInt a) {
+	QInt c;
+	c.dec = congHaiSoHe10(this->dec, a.dec);
+	c.binary = StrToBi(c.dec);
+	string c1, c2, c3, c4;
+	if (c.binary.length() < 32) {
+		for (int i = 0; i < c.binary.length(); i++) {
+			c.data[0] = c.data[0] | atoi(c.binary.substr(i, 1).c_str()) << (32 - 1 - i);
+		}
+	}
+	else
+		if (c.binary.length() >= 32 && c.binary.length() < 64) {
+			c1 = c.binary.substr(0, 31);
+			c2 = c.binary.substr(31, c.binary.length() - 31);
+			cout << c1 << endl;
+			cout << c2 << endl;
+			for (int i = 0; i < c1.length(); i++) {
+				c.data[0] = c.data[0] | atoi(c1.substr(i, 1).c_str()) << (32 - 1 - i);
+			}
+			for (int j = 0; j < c2.length(); j++) {
+				c.data[1] = c.data[1] | atoi(c2.substr(j, 1).c_str()) << (32 - 1 - j);
+			}
+		}
+		else
+			if (c.binary.length() >= 64 && c.binary.length() < 96) {
+				c1 = c.binary.substr(0, 31);
+				c2 = c.binary.substr(31, 31);
+				c3 = c.binary.substr(62, c.binary.length() - 62);
+				cout << "c1:" << c1 << endl;
+				cout << "c2:" << c2 << endl;
+				cout << "c3:" << c3 << endl;
+				for (int i = 0; i < c1.length(); i++) {
+					c.data[0] = c.data[0] | atoi(c1.substr(i, 1).c_str()) << (32 - 1 - i);
+				}
+				for (int j = 0; j < c2.length(); j++) {
+					c.data[1] = c.data[1] | atoi(c2.substr(j, 1).c_str()) << (32 - 1 - j);
+				}
+				for (int p = 0; p < c3.length(); p++) {
+					c.data[2] = c.data[2] | atoi(c3.substr(p, 1).c_str()) << (32 - 1 - p);
+				}
+			}
+			else
+				if (c.binary.length() >= 96 && c.binary.length() < 128) {
+					c1 = c.binary.substr(0, 31);
+					c2 = c.binary.substr(31, 31);
+					c3 = c.binary.substr(62, 31);
+					c4 = c.binary.substr(94, c.binary.length() - 93);
+					cout << "c1:" << c1 << endl;
+					cout << "c2:" << c2 << endl;
+					cout << "c3:" << c3 << endl;
+					cout << "c4:" << c4 << endl;
+					for (int i = 0; i < c1.length(); i++) {
+						c.data[0] = c.data[0] | atoi(c1.substr(i, 1).c_str()) << (32 - 1 - i);
+					}
+					for (int j = 0; j < c2.length(); j++) {
+						c.data[1] = c.data[1] | atoi(c2.substr(j, 1).c_str()) << (32 - 1 - j);
+					}
+					for (int p = 0; p < c3.length(); p++) {
+						c.data[2] = c.data[2] | atoi(c3.substr(p, 1).c_str()) << (32 - 1 - p);
+					}
+					for (int w = 0; w < c4.length(); w++) {
+						c.data[3] = c.data[3] | atoi(c4.substr(w, 1).c_str()) << (32 - 1 - w);
+					}
+				}
+				else
+					cout << "Vuot qua kha nang xu ly." << endl;
+	return c;
 };
+//QInt QInt::operator+(const QInt &a) {
+//	QInt b;
+//	return b;
+//};
 QInt QInt::operator-(const QInt &a) {
 	QInt b;
 	return b;
@@ -49,9 +116,11 @@ void ScanQInt(QInt &z) {
 	cout << "nhap chuoi:";
 	string b;
 	getline(cin, b);
+	z.dec = b;// gan chuoi he 10
 	string c = StrToBi(b);
-	cout << c << endl;
-
+	cout << "quy doi sang he 2:" << c << endl;
+	// gan chuoi binary
+	z.binary = c;
 	// xuoi
 	cout << "Do dai chuoi:" << b.length() << endl;
 	cout << "Do dai day bit:" << c.length() << endl;
@@ -121,9 +190,12 @@ void ScanQInt(QInt &z) {
 };
 
 void PrintQInt(QInt &z) {
+	cout << "tong quat:" << endl;
 	for (int j = 0; j < 4; j++) {
+		cout << "[" << j << "]:" << endl;
 		for (int i = 0; i < 32; i++) {
 			cout << ((z.data[j] >> (32 - i)) & 1);
 		}
+		cout << endl;
 	}
 }
--- DoAn/DoAn/main.cpp
+++ DoAn/DoAn/main.cpp
@@ -6,7 +6,25 @@
 int main() {
 	QInt a;
 	ScanQInt(a);
+	cout << endl;
 	PrintQInt(a);
+	cout << endl;
+	QInt b;
+	b = a;
+	//cout << endl;
+	//PrintQInt(b);
+	QInt c;
+	ScanQInt(c);
+	cout << endl;
+	PrintQInt(c);
+	cout << endl;
+	//string k=congHaiSoHe10(a.dec, c.dec);
+	c = c + a;
+	cout << "ket qua:";
+	PrintQInt(c);
+	/*string c = "10423423423529";
+	string d = "31231231";
+	cout<<congHaiSoHe10(c, d);*/
 	system("pause");
 	return 0;
 }
