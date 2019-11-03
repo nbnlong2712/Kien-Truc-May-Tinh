cfb184db9c66dc0a7ef42932f136ae0200b40bb3
BaoSon <lqBaoSon@outlook.com>
2019-11-04 01:38:33 +0700

    so sanh/xoay/dich/fix nhap xuat/+ -

--- DoAn/DoAn/QInt.cpp
+++ DoAn/DoAn/QInt.cpp
@@ -6,6 +6,16 @@ QInt::QInt() {
 		data[i] = { 0 };
 	}
 };
+QInt daobitbu2(QInt &a) {
+	QInt b;
+	b.data[3] = b.data[3] | (1 << 0);
+	//PrintQInt(b);
+	for (int i = 0; i < 4; i++) {
+		a.data[i] = ~a.data[i];
+	}
+	a = a + b;
+	return a;
+}
 QInt QInt::operator=(const QInt &a) {
 	QInt c;
 	for (int i = 0; i < 4; i++) {
@@ -13,7 +23,50 @@ QInt QInt::operator=(const QInt &a) {
 	}
 	return *this;
 };
+QInt QInt::operator+(QInt a) {
+	int nho=0;
+	QInt c;
+	for (int i = 3; i >=0; i--) {
+		for (int j = 31; j >=0; j--) {
+			if ((((data[i] >> (32 - j-1)) & 1) + ((a.data[i] >> (32 - j - 1)) & 1)+nho )== 2) {
+				nho = 1;
+				c.data[i] = c.data[i] | (0 << (32 - j - 1));
+			}else
+				if ((((data[i] >> (32 - j - 1)) & 1) + ((a.data[i] >> (32 - j - 1)) & 1) + nho) == 1) {
+					c.data[i] = c.data[i] | (1 << (32 - j - 1));
+					nho = 0;
+				}
+				else
+					if ((((data[i] >> (32 - j - 1)) & 1) + ((a.data[i] >> (32 - j - 1)) & 1) + nho) == 0) {
+						c.data[i] = c.data[i] | (0 << (32 - j - 1));
+						nho = 0;
+					}
+					else
+						if ((((data[i] >> (32 - j - 1)) & 1) + ((a.data[i] >> (32 - j - 1)) & 1) + nho) == 3) {
+							nho = 1;
+							c.data[i] = c.data[i] | (0 << (32 - j - 1));
+						}
 
+		}
+	}
+	return c;
+};
+QInt QInt::operator-(QInt a) {
+	QInt c;
+	QInt k = daobitbu2(a);
+	c=*this+k;
+	return c;
+};
+//QInt QInt::operator*(QInt a) {
+//	QInt b;
+//	return b;
+//
+//};
+//QInt QInt::operator/(QInt a) {
+//	QInt b;
+//	return b;
+//
+//};
 QInt QInt::operator&( QInt a) {
 	QInt c;
 	for (int i = 0; i < 4; i++) {
@@ -55,16 +108,21 @@ void ScanQInt(QInt &z) {
 	cout << "nhap chuoi:";
 	string b;
 	getline(cin, b);
-	//z.dec = b;// gan chuoi he 10
+	/*if (b.substr(0,1) == "-") {
+		cout << "kkk";
+		z.data[0]=z.data[0]|(1<<32);
+		b = b.substr(1, b.length() - 1);
+	}*/
+
 	string c = StrToBi(b);
 	while (c.length() != 128) {
 		c = '0' + c;
 	}
 	int r = 0;
-	//cout << "quy doi sang he 2:"<<endl << c << endl;
+
 	for (int i = 0; i < 4; i++) {
 		for (int j = 0; j < 32; j++) {
-			z.data[i] = z.data[i] | atoi(c.substr(r, 1).c_str()) << (32 - j);
+			z.data[i] = z.data[i] | atoi(c.substr(r, 1).c_str()) << (32 - j-1);
 			r++;
 		}
 	}
@@ -75,36 +133,146 @@ void PrintQInt(QInt &z) {
 	for (int j = 0; j < 4; j++) {
 		//cout << "[" << j << "]:" << endl;
 		for (int i = 0; i < 32; i++) {
-			cout << ((z.data[j] >> (32 - i)) & 1);
+			cout << ((z.data[j] >> (32 - i-1)) & 1);
 		}
 	}
 	cout << endl;
 }
-QInt QInt::operator+(QInt a) {
-	int nho=0;
+//void ShiftRight(int &c,int a[32],int q[32]) {
+//	for (int i = 0; i < n; i++) {
+//		a.data[3] >> 1;
+//	}
+//};
+bool operator ==(QInt a, QInt b) {
+	int dem = 0;
 	QInt c;
-	for (int i = 3; i >=0; i--) {
-		for (int j = 32; j >=0; j--) {
-			if ((((data[i] >> (32 - j)) & 1) + ((a.data[i] >> (32 - j)) & 1)+nho )== 2) {
-				nho = 1;
-				c.data[i] = c.data[i] | (0 << (32 - j));
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 32; j++) {
+			if ((a.data[i] >> (32 - 1 - j)) == (b.data[i] >> (32 - 1 - j))) {
+				dem++;
+			}
+		}
+	}
+	if (dem == 128)
+		return true;
+	else
+		return false;
+}
+bool operator >(QInt a, QInt b) {
+	for (int i = 0; i <4; i++) {
+		for (int j = 0; j <32; j++) {
+			if (((a.data[i] >> (32 - j-1))) > ((b.data[i] >> (32 - j-1)))) {
+				return true;
 			}else
-				if ((((data[i] >> (32 - j)) & 1) + ((a.data[i] >> (32 - j)) & 1) + nho) == 1) {
-					c.data[i] = c.data[i] | (1 << (32 - j));
-					nho = 0;
+				if (((a.data[i] >> (32 - j-1))) < ((b.data[i] >> (32 - j-1)))) {
+					return false;
 				}
-				else
-					if ((((data[i] >> (32 - j)) & 1) + ((a.data[i] >> (32 - j)) & 1) + nho) == 0) {
-						c.data[i] = c.data[i] | (0 << (32 - j));
-						nho = 0;
-					}
-					else
-						if ((((data[i] >> (32 - j)) & 1) + ((a.data[i] >> (32 - j)) & 1) + nho) == 3) {
-							nho = 1;
-							c.data[i] = c.data[i] | (0 << (32 - j));
-						}
+		}
+	}
+	return false;
+
+}
+
+bool operator <(QInt a, QInt b) {
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 32; j++) {
+			if (((a.data[i] >> (32 - j-1))) < ((b.data[i] >> (32 - j-1)))) {
+				return true;
+			}
+			else
+				if (((a.data[i] >> (32 - j-1))) > ((b.data[i] >> (32 - j-1)))) {
+					return false;
+				}
+		}
+	}
+	return false;
+}
+
+bool operator >=(QInt a, QInt b) {
+	if (a > b||a == b) {
+		return true;
+	}
+	return false;
+}
+bool operator <=(QInt a, QInt b) {
+	if (a < b || a == b) {
+		return true;
+	}
+	return false;
+}
+void DichTrai(QInt &a) {
+	int temp[128];
+	int k = 0;
+	// tach ra 2 doan
+	for (int i = 0; i < 31; i++) {
+		temp[k] = (a.data[0] >> (31 - 1 - i)) & 1;
+		k++;
+	}
+	for (int j = 1; j < 4; j++) {
+			for (int i = 0; i < 32; i++) {
+				temp[k] = (a.data[j] >> (32 - 1 - i)) & 1;
+				k++;
+			}
+	}
+	temp[127] = 0;
+	for (int i = 0; i < 4; i++) {
+		a.data[i] = { 0 };
+	}
+	cout << endl;
+	/*for (int i = 0; i < 128; i++) {
+		cout << temp[i];
+	}*/
+	cout << endl;
+	int r = 0;
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 32; j++) {
+			a.data[i] = a.data[i] |temp[r] << (32 - j-1);
+			r++;
+		}
+	}
+
+}
+void DichPhai(QInt &a) {
+	int temp[128];
+	temp[0] = 0;
+	int k = 1;
+	for (int j = 0; j < 4; j++) {
+		for (int i = 0; i < 32; i++) {
+			if (k < 128) {
+				temp[k] = ((a.data[j] >> (32-1 - i)) & 1);
+				k++;
+			}
 
 		}
 	}
-	return c;
-};
+	for (int i = 0; i < 4; i++) {
+		a.data[i] = { 0 };
+	}
+	//for (int i = 0; i < 128; i++) {
+	//	cout << temp[i];
+	//}
+	int r = 0;
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 32; j++) {
+			a.data[i] = a.data[i] | temp[r] << (32 - j-1);
+			r++;
+		}
+	}
+
+}
+
+void rol(QInt &a) {
+	int luu;
+	luu= ((a.data[0] >> (32 - 1)) & 1);
+	DichTrai(a);
+	a.data[3] = a.data[3] | (luu << 32);
+};// xoay trai
+
+void ror(QInt &a) {
+	int luu;
+	luu = ((a.data[3] >> (0)) & 1);
+	cout << endl;
+	//cout << luu; cout << endl;
+	DichPhai(a);
+	a.data[0] = a.data[0] | (luu << (32-1));
+};// xoay trai
